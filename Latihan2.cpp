70b35df1b2fdbcb6a6538ea422dad6cbd5dde73b
Yazrol Surya <yazrol.surya60@gmail.com>
2026-04-01 18:50:57 +0700

    Latihan 2 Materi Increment

--- Latihan2.cpp
+++ Latihan2.cpp
@@ -11,17 +11,3 @@ int main() {
 		cout << "Nilai ke-"<< x++ << ": " << ujian[indeks++] << endl;
 	}
 }
-=======
-#include <iostream>
-using namespace std;
-
-int main() {
-	int ujian[] = {85, 90, 78, 92, 88};
-	int indeks = 0;
-	int x = 1;
-	
-	while(ujian[indeks] >= 4 && x <=  5) {
-		cout << "Nilai ke-"<< x++ << ": " << ujian[indeks++] << endl;
-	}
-}
->>>>>>> b2f31a2dcc913ae7f74997a01923696e9397b55f
