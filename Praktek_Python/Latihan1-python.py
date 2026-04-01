b40a8d74d167461ad52d2a177349260dce330e52
Yazrol Surya <yazrol.surya60@gmail.com>
2026-04-01 16:04:42 +0700

    Folder Python

---
+++ Praktek_Python/Latihan1-python.py
@@ -0,0 +1,67 @@
+# Mencetak nilai ke layar
+print("a")
+
+# Mengisi Nilai ke dalam variabel
+x = 5
+print(x)
+
+# Melakukan Indentasi
+
+# Versi benar 
+if 6 > 3 :
+     print("Enam Lebih Besar dari Tiga")
+
+if x > 5 :
+     print("Nilai x lebih dari 5")
+     if y < 10:
+          print("Nilai y kurang dari 10")
+
+# Versi salah
+"""
+if x > 5 :
+print("Nilai x lebih dari 5")
+if y < 10:
+print("Nilai y kurang dari 10")
+
+
+if x > 5 :
+     print("Nilai x lebih dari 5")
+print("Tidak sesuai dengan aturan Indentasi")
+
+if y < 10:
+print("Nilai y kurang dari 10")
+     print("Tidak sesuai dengan aturan Indentasi")
+"""
+
+# Ini adalah komentar satu baris
+print("coba")
+
+# Ini baris komentar
+# Masih di baris kedua komentar
+# ini baris komentar ketiga
+print("coba")
+
+'''
+ini adalah
+komentar
+multibaris
+'''
+print("coba")
+
+"""
+ini contoh lain
+barisan
+beberapa Komentar
+dengan tanda petik ganda
+"""
+print("coba")
+
+# Komentar Dokumentasi
+def fungsi():
+     """
+     ini adalah dokumentasi fungsi
+     """
+     pass
+
+# Komentar untuk menonaktifkan Kode
+# print("Ini adalah baris kode yang di nonaktifkan")
