6009f5b0ff5518b8fdb6e4f49207a657dbc5890b
Yazrol Surya <yazrol.surya60@gmail.com>
2026-04-04 13:42:22 +0700

    Latihan 9 Javascript

---
+++ Praktek_Javascript/Latihan9-Javascript.js
@@ -0,0 +1,10 @@
+let age = 25;
+let Age = 30;
+
+console.log(age); // 25
+console.log(Age); // 30
+
+// Valid Names Variable
+let thisIsCamelCase;
+let anotherExampleVariable;
+let freeCodeCampStudents;
