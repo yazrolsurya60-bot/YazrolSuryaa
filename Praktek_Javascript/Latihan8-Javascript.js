ed3942d93c49c4eed5257434087125a94c801b98
Yazrol Surya <yazrol.surya60@gmail.com>
2026-04-04 13:41:41 +0700

    Latihan 8 Javascript

---
+++ Praktek_Javascript/Latihan8-Javascript.js
@@ -0,0 +1,14 @@
+// Bad Variable Names
+let x = 10;
+let y = "John";
+
+// Good Variable Names
+let umur = 10;
+
+// Valid Variable Names
+let age;
+let _score;
+let $total;
+
+// Invalid Variable names
+// let 1stPlace; // Start with a numbers
