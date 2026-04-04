f2edb18b7cfcc3bb6b1852bdba0cbf17c3f7ee75
Yazrol Surya <yazrol.surya60@gmail.com>
2026-04-04 10:56:00 +0700

    Latihan 6 Javascript

---
+++ Praktek_Javascript/Latihan6-Javascript.js
@@ -0,0 +1,15 @@
+let User = {
+  name: "Alice",
+  age: 30,
+};
+
+console.log(User);
+console.log(typeof User);
+
+Symbol("mySymbol");
+console.log(Symbol);
+
+let bigInt = 123456789012345678901234567890n;
+
+console.log(bigInt);
+console.log(typeof bigInt);
