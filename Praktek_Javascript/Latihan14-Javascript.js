1e2b994ec3f5953573432f37a4cad1466d1c14f7
Yazrol Surya <yazrol.surya60@gmail.com>
2026-04-06 23:51:28 +0700

    Latihan 14 Javascript

---
+++ Praktek_Javascript/Latihan14-Javascript.js
@@ -0,0 +1,7 @@
+// const improperStr = "Do not do this';
+// Contoh di atas adalah cara yang salah
+
+let developer = "Jessica";
+console.log(developer);
+developer = "Quincy";
+console.log(developer);
