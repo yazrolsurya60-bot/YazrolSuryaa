014300e75acd294be3c5c2566f941632afb66a8a
Yazrol Surya <yazrol.surya60@gmail.com>
2026-04-08 23:59:39 +0700

    Latihan 22 Javascript

--- Praktek_Javascript/Latihan22-Javascript.js
+++ Praktek_Javascript/Latihan22-Javascript.js
@@ -3,9 +3,11 @@ let variableTwo = 10; // Declare another variable and assign a value
 
 let a = 1; // Statement ends here
 let b = 2; // Another statement starts here
+let c = 3;
 
 console.log(variableOne);
 console.log(variableTwo);
 
 console.log(a);
 console.log(b);
+console.log(c);
